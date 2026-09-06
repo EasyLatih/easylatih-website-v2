f82fc34da1f488a32221fbcba4c8a9d0bed03fc7
EasyLatih <admin@easylatih.my>
2026-09-06 17:28:19 +0800

    Add trainer portal configuration scaffold

---
+++ trainer/config.js
@@ -0,0 +1,10 @@
+window.EASYLATIH_TRAINER_PORTAL = {
+  supabaseUrl: "",
+  supabasePublishableKey: "",
+  appName: "EasyLatih Trainer Collaboration",
+  privacyNoticeVersion: "2026-09-06",
+  collaborationTermsVersion: "2026-09-06",
+  maxActiveProposals: 5,
+  supportEmail: "sales@easylatih.my",
+  supportWhatsApp: "+60109202811"
+};
