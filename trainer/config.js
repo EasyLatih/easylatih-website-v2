1c42afb2e8d2b54d292be855b9a2354dc7df1f01
EasyLatih <admin@easylatih.my>
2026-09-09 11:27:47 +0800

    Load TTT exemption evidence UI sync

--- trainer/config.js
+++ trainer/config.js
@@ -17,9 +17,11 @@ if (/\/trainer\/dashboard(?:\.html)?$/.test(window.location.pathname)) {
   document.write('<script src="onboarding-ttt-override.js"><\/script>');
   document.write('<script src="onboarding-save-fix.js"><\/script>');
   document.write('<script src="onboarding-accordion.js"><\/script>');
+  document.write('<script src="ttt-evidence-sync.js"><\/script>');
 }
 
 if (/\/internal\/trainer-admin(?:\.html)?$/.test(window.location.pathname)) {
   document.write('<script src="trainer-admin-course-outline.js"><\/script>');
   document.write('<script src="trainer-admin-programme-editor.js"><\/script>');
+  document.write('<script src="trainer-admin-ttt-evidence-sync.js"><\/script>');
 }
