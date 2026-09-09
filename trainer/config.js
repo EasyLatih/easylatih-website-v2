4d27309fe76401cf32431b62ce8bb3bd0f748cf2
EasyLatih <admin@easylatih.my>
2026-09-09 10:21:21 +0800

    Load trainer journey checklist and tab badges

--- trainer/config.js
+++ trainer/config.js
@@ -13,6 +13,7 @@ if (/\/trainer\/dashboard(?:\.html)?$/.test(window.location.pathname)) {
   document.write('<script src="etris-drive-documents.js"><\/script>');
   document.write('<script src="programme-course-outline.js"><\/script>');
   document.write('<script src="programme-admin-changes.js"><\/script>');
+  document.write('<script src="journey-notifications.js"><\/script>');
 }
 
 if (/\/internal\/trainer-admin(?:\.html)?$/.test(window.location.pathname)) {
