8e4e9ec68b5a9e19e0a4e0b8e8ec2e0825778b11
EasyLatih <admin@easylatih.my>
2026-09-09 13:52:38 +0800

    Load admin training request shortcut

--- trainer/config.js
+++ trainer/config.js
@@ -29,4 +29,5 @@ if (/\/internal\/trainer-admin(?:\.html)?$/.test(window.location.pathname)) {
   document.write('<script src="trainer-admin-pools.js"><\/script>');
   document.write('<script src="../internal/trainer-admin-search.js"><\/script>');
   document.write('<script src="../internal/trainer-admin-workspace.js"><\/script>');
+  document.write('<script src="../internal/trainer-admin-opportunity-shortcut.js"><\/script>');
 }
